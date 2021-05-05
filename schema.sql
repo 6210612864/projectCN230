cb0033673b6d9b4a0b0f5238e102d3a316fa583e
Raned Chuphueak <6210612864@student.tu.ac.th>
2021-05-06 04:40:37 +0700

    insert some data to table (incomplete)

--- schema.sql
+++ schema.sql
@@ -38,7 +38,6 @@ create table Invoice
 (
 	InvoiceID int,
 	Cost float not null,
-	Date varchar(50) null,
 	constraint Invoice_pk
 		primary key (InvoiceID)
 );
@@ -76,6 +75,7 @@ create table Assign_in
 (
     pID int not null,
     invoiceID int not null,
+    Date varchar(50) null,
     constraint Assign_in_Product_pID_fk
         foreign key (pID) references Product (pID),
     constraint Assign_in_Invoice_invoiceID_fk
@@ -125,24 +125,80 @@ insert into Journal values ( 5, 'Purchase return journal', 'return/income');
 insert into Journal values ( 6, 'Sales return journal', 'return/disbursement');
 insert into Journal values ( 7, 'Journal proper/General journal', 'general');
 
-insert into Invoice values ( 500001, 50000000, '18 Nov 2020', 146427, 1, 2);
-insert into Invoice values ( 500002, 987200000, '19 Nov 2020',146427, 6, 2);
-insert into Invoice values ( 1, 20000000, '25 Nov 2020', 570162, 4, 2);
-insert into Invoice values ( 100001, 1925040000, '08 Dec 2020',987657, 6, 2);
-insert into Invoice values ( 100002, 58500000, '09 Dec 2020', 987657, 2, 2);
-insert into Invoice values ( 100003, 195000000, '10 Dec 2020', 987657, 4, 3);
-insert into Invoice values ( 2 ,30400000, '17 Dec 2020', 570162, 1, 2);
-insert into Invoice values ( 100004, 195000000, '21 Dec 2020', 987657, 3, 3);
-insert into Invoice values ( 200001, 15000000, '06 Jan 2021', 119423, 3, 2);
-insert into Invoice values ( 3, 27000000, '29 Jan 2021', 570162, 3, 2);
-insert into Invoice values ( 100005, 195000000 , '03 Feb 2021', 987657, 1, 3);
-insert into Invoice values ( 200002, 30000000, '04 May 2021',119423, 2, 2);
-insert into Invoice values ( 500003, 6000000, '04 Feb 2021',  146427, 4, 2);
-insert into Invoice values ( 500004, 15000000, '04 Feb 2021',  146427, 4, 3);
-insert into Invoice values ( 4, 11100000, '10 Feb 2021',570162, 5, 2);
-insert into Invoice values ( 100006, 28860000, '10 Feb 2021',987657, 5, 2);
-insert into Invoice values ( 200003, 60000000, '13 Feb 2021', 119423, 4, 2);
-insert into Invoice values ( 100007, 39000000, '05 April 2021', 987657, 2, 2);
-insert into Invoice values ( 500005, 3000000, '24 April 2021', 146427, 2, 2);
-insert into Invoice values ( 200004, 1234000000, '24 April 2021',119423, 6, 3);
-insert into Invoice values ( 100008, 288600000, '05 May 2021',987657, 5, 3);
+insert into Invoice values ( 500001, 50000000, 146427, 1, 2);
+insert into Invoice values ( 500002, 987200000, 146427, 6, 2);
+insert into Invoice values ( 1, 20000000, 570162, 4, 2);
+insert into Invoice values ( 100001, 1925040000, 987657, 6, 2);
+insert into Invoice values ( 100002, 58500000, 987657, 2, 2);
+insert into Invoice values ( 100003, 195000000, 987657, 4, 3);
+insert into Invoice values ( 2 ,30400000, 570162, 1, 2);
+insert into Invoice values ( 100004, 195000000, 987657, 3, 3);
+insert into Invoice values ( 200001, 15000000, 119423, 3, 2);
+insert into Invoice values ( 3, 27000000, 570162, 3, 2);
+insert into Invoice values ( 100005, 195000000 , 987657, 1, 3);
+insert into Invoice values ( 200002, 30000000, 119423, 2, 2);
+insert into Invoice values ( 500003, 6000000,  146427, 4, 2);
+insert into Invoice values ( 500004, 15000000,  146427, 4, 3);
+insert into Invoice values ( 4, 11100000, 570162, 5, 2);
+insert into Invoice values ( 100006, 28860000, 987657, 5, 2);
+insert into Invoice values ( 200003, 60000000, 119423, 4, 2);
+insert into Invoice values ( 5, 305000000, 570162,7,3);
+insert into Invoice values ( 300001, 120000000, 735816,7,2);
+insert into Invoice values ( 100007, 39000000, 987657, 2, 2);
+insert into Invoice values ( 500005, 3000000, 146427, 2, 2);
+insert into Invoice values ( 200004, 1234000000, 119423, 6, 3);
+insert into Invoice values ( 300002, 60000000, 735816,7,2);
+insert into Invoice values ( 100008, 288600000, 987657, 5, 3);
+insert into Invoice values ( 300003, 12000000, 735816,7,2);
+
+insert into Buy values ( 1, 6);
+insert into Buy values ( 6, 6);
+insert into Buy values ( 4, 1);
+insert into Buy values ( 6, 2);
+insert into Buy values ( 2, 2);
+insert into Buy values ( 4, 2);
+insert into Buy values ( 1, 1);
+insert into Buy values ( 3, 2);
+insert into Buy values ( 3, 3);
+insert into Buy values ( 3, 1);
+insert into Buy values ( 1, 2);
+insert into Buy values ( 2, 3);
+insert into Buy values ( 4, 6);
+insert into Buy values ( 4, 6);
+insert into Buy values ( 5, 1);
+insert into Buy values ( 5, 2);
+insert into Buy values ( 4, 3);
+insert into Buy values ( 7, 1);
+insert into Buy values ( 7, 4);
+insert into Buy values ( 2, 2);
+insert into Buy values ( 2, 6);
+insert into Buy values ( 6, 3);
+insert into Buy values ( 7, 4);
+insert into Buy values ( 5, 2);
+insert into Buy values ( 7, 4);
+
+insert into Assign_in values (6, 500001, '18 Nov 2020');
+insert into Assign_in values (6, 500002, '19 Nov 2020');
+insert into Assign_in values (1, 1, '25 Nov 2020');
+insert into Assign_in values (2, 100001, '08 Dec 2020');
+insert into Assign_in values (2, 100002, '09 Dec 2020');
+insert into Assign_in values (2, 100003, '10 Dec 2020');
+insert into Assign_in values (1, 2 , '17 Dec 2020');
+insert into Assign_in values (2, 100004, '21 Dec 2020');
+insert into Assign_in values (2, 200001, '06 Jan 2021');
+insert into Assign_in values (1, 3, '29 Jan 2021');
+insert into Assign_in values (2, 100005, '03 Feb 2021');
+insert into Assign_in values (3, 200002, '04 May 2021');
+insert into Assign_in values (6, 500003, '04 Feb 2021');
+insert into Assign_in values (6, 500004, '04 Feb 2021');
+insert into Assign_in values (1, 4, '10 Feb 2021');
+insert into Assign_in values (2, 100006, '10 Feb 2021');
+insert into Assign_in values (2, 200003, '13 Feb 2021');
+insert into Assign_in values (1, 5, '17 Feb 2021');
+insert into Assign_in values (4, 300001, '17 Feb 2021');
+insert into Assign_in values (2, 100007, '05 April 2021');
+insert into Assign_in values (6, 500005, '24 April 2021');
+insert into Assign_in values (3, 200004, '24 April 2021');
+insert into Assign_in values (4, 300002, '30 April 2021');
+insert into Assign_in values (2, 100008, '05 May 2021');
+insert into Assign_in values (4, 300003, '22 May 2021');
